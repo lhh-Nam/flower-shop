ed12019abf2ce138e75cdd891dbdf614a2f4a2ad
HuyNam <>
2021-04-25 22:57:49 +0700

    add Login - Logout - Infor User

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -47,6 +47,40 @@ const userController = {
     }
   },
 
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await Users.findOne({ email });
+      if (!user) return res.status(400).json({ msg: "User does not exist." });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ msg: "Incorrect password." });
+
+      //If Login success, create token and refresh token
+      const accesstoken = createAccessToken({ id: user._id });
+      const refreshtoken = createRefreshToken({ id: user._id });
+
+      res.cookie("refreshtoken", refreshtoken, {
+        httpOnly: true,
+        path: "/user/refresh_token",
+      });
+
+      res.json({ accesstoken });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+
+  logout: async (req, res) => {
+    try {
+      res.clearCookie("refreshtoken", { path: "/user/refresh_token" });
+      return res.json({ msg: "Logged out" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+
   refreshToken: (req, res) => {
     try {
       const rf_token = req.cookies.refreshtoken;
@@ -56,14 +90,25 @@ const userController = {
 
       jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
         if (err)
-          return res.status(400).json({ msg: "Please Login or Register" });     
+          return res.status(400).json({ msg: "Please Login or Register" });
         const accesstoken = createAccessToken({ id: user.id });
         res.json({ user, accesstoken });
       });
 
       res.json({ rf_token });
     } catch (error) {
-      return res.status(500).json({ msg: err.message });
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+
+  getUser: async (req, res) => {
+    try {
+      const user = await Users.findById(req.user.id).select("-password");
+      if (!user) return res.status(400).json({ msg: "USer does not exist." });
+
+      res.json(user);
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
     }
   },
 };
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 const userController = require("../controllers/user.controller");
+const auth = require("../middleware/auth");
 
 router.post("/register", userController.register); // xác định controller =>> register
+
+router.post("/login", userController.login); // xác định controller =>> login
+
+router.get("/logout", userController.logout); // xác định controller =>> login
+
 router.get("/refresh_token", userController.refreshToken); // xác định controller =>> register
+
+router.get('/infor', auth, userController.getUser)
 
 module.exports = router;
